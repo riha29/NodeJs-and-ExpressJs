4953ff61aa7a9b857f9052d436409453fa5bd71f
Faozia Islam Riha <96914762+riha29@users.noreply.github.com>
2023-11-15 01:13:08 +0600

    modules/npm

--- app.js
+++ app.js
@@ -1,26 +1,5 @@
-// const amount = 12;
+const _ = require("lodash");
 
-// if (amount < 10) {
-//   console.log("small number");
-// } else {
-//   console.log("large number");
-// }
-
-// console.log(`hey it's my first node app!!!`);
-
-// const secret = "SUPER SECRET";
-// const john = "john";
-// const peter = "peter";
-// const sayHi = (name) => {
-//   console.log(`Hello there ${name}`);
-// };
-
-const names = require("./4-names");
-const sayHi = require("./5-utils");
-const data = require("./6-alternative-flavor");
-require("./7-mind-grenade");
-// sayHi("susan");
-// // sayHi(john);
-// // sayHi(peter);
-// sayHi(names.john);
-// sayHi(names.peter);
+const items = [1, [2, [3, [4]]]];
+const newItems = _.flattenDeep(items);
+console.log(newItems);
