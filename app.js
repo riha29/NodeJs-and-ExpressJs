c16c42e05030019a8be96f55708ec0a613984be4
Faozia Islam Riha <96914762+riha29@users.noreply.github.com>
2023-11-17 00:38:48 +0600

    http/express

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('Welcome to Node Tutorial')
+const express = require("express");
+const path = require("path");
+const app = express();
+
+//setup static and middleware
+app.use(express.static("./public"));
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+// adding to static assests
+// SSR
+// });
+
+app.all("*", (req, res) => {
+  res.status(404).send("resource not found");
+});
+
+app.listen(5000, () => {
+  console.log("server is listening to port 5000");
+});
