4e6e001dff4b0f4be03a1533ad9f9475cccaa75b
Faozia Islam Riha <96914762+riha29@users.noreply.github.com>
2023-11-20 23:56:01 +0600

    updaet

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 const express = require("express");
-const path = require("path");
 const app = express();
+const { products } = require("./data");
 
-//setup static and middleware
-app.use(express.static("./public"));
+app.get("/", (req, res) => {
+  res.send("<h1> Home Page </h1> <a href='/api/products'> Products </a>");
+});
 
-// app.get("/", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
-// adding to static assests
-// SSR
-// });
+app.get("/api/products/:productID", (req, res) => {
+  // console.log(req);
+  // console.log(req.params);
+  const { productID } = req.params;
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  );
+  if (!singleProduct) {
+    return res.status(404).send("Product Does Not Exist");
+  }
+  res.json(singleProduct);
+});
 
-app.all("*", (req, res) => {
-  res.status(404).send("resource not found");
+app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
+  console.log(req.params);
+  res.send("hello world");
 });
 
 app.listen(5000, () => {
